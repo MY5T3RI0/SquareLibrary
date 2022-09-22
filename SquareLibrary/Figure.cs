9b3712b8a154b527d2751a919e4a3dfd5fdb064a
MY5T3RI0 <66646109+MY5T3RI0@users.noreply.github.com>
2022-09-22 14:02:33 +0300

    Доработан класс "Фигура".

--- SquareLibrary/Figure.cs
+++ SquareLibrary/Figure.cs
@@ -3,12 +3,25 @@ using System.Collections.Generic;
 
 namespace SquareLibrary
 {
+    /// <summary>
+    /// Фигура.
+    /// </summary>
     public abstract class Figure
     {
-        protected List<int> Sides { get; set; }
+        /// <summary>
+        /// Список сторон.
+        /// </summary>
+        private List<int> Sides { get; set; }
 
-        protected int Radius { get; set; }
+        /// <summary>
+        /// Радиус.
+        /// </summary>
+        private int Radius { get; set; }
 
+        /// <summary>
+        /// Создать новую фигуру.
+        /// </summary>
+        /// <param name="radius">Радиус.</param>
         public Figure(int radius)
         {         
             if (radius <= 0)
@@ -19,16 +32,28 @@ namespace SquareLibrary
             Radius = radius;
         }
 
+        /// <summary>
+        /// Создать новую фигуру.
+        /// </summary>
+        /// <param name="sides">Список сторон.</param>
         public Figure(List<int> sides)
         {
             Sides = sides ?? throw new ArgumentNullException("Стороны не могут быть null", nameof(sides));
         }
 
+        /// <summary>
+        /// Получить периметр.
+        /// </summary>
+        /// <returns>Периметр.</returns>
         public virtual int GetPerimeter()
         {
             return default;  
         }
 
+        /// <summary>
+        /// Получить площадь.
+        /// </summary>
+        /// <returns>Площадь.</returns>
         public virtual double GetSquare()
         {
             return default;

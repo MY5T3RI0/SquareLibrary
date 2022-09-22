4d2a383f9e9a42749229abf4443bed45eb8ab0f8
MY5T3RI0 <66646109+MY5T3RI0@users.noreply.github.com>
2022-09-22 13:53:32 +0300

    Добавьте файлы проекта.

---
+++ SquareLibrary/Figure.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SquareLibrary
+{
+    public abstract class Figure
+    {
+        protected List<int> Sides { get; set; }
+
+        protected int Radius { get; set; }
+
+        public Figure(int radius)
+        {         
+            if (radius <= 0)
+            {
+                throw new ArgumentException("Радиус не может быть <= 0", nameof(radius));
+            }
+
+            Radius = radius;
+        }
+
+        public Figure(List<int> sides)
+        {
+            Sides = sides ?? throw new ArgumentNullException("Стороны не могут быть null", nameof(sides));
+        }
+
+        public virtual int GetPerimeter()
+        {
+            return default;  
+        }
+
+        public virtual double GetSquare()
+        {
+            return default;
+        }
+    }
+}
